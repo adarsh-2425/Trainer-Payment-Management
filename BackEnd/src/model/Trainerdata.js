0aa36d7a11e2809968b667ec680e6cf3c7a6e491
sibajohn <102389770+sibajohn@users.noreply.github.com>
2022-09-05 11:42:54 +0100

    Trainerdata file added

---
+++ BackEnd/src/model/Trainerdata.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://admin:mongo@group4.8ajo2cj.mongodb.net/paymentDB?retryWrites=true&w=majority';
+
+const connectionParams={
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}
+
+mongoose.connect(uri,connectionParams)
+  .then( () => {
+      console.log('Connected to the User atlas database ')
+  })
+  .catch( (err) => {
+      console.error(`Error connecting to the database. n${err}`);
+  })
+
+// creating schema
+const Schema = mongoose.Schema;
+
+var TrainerDataSchema = new Schema({
+    Name : String,
+    Batch : String,
+    Date: Date,
+    Email : {
+      type : String,
+      lowercase : true
+    },
+    Program:String,
+    Sessions :Number
+    
+});
+// creating model
+var Trainerdata = mongoose.model('Trainer', TrainerDataSchema);                        //UserData is the model and NewBookData is the schema
+
+// exporting model
+module.exports = Trainerdata;
