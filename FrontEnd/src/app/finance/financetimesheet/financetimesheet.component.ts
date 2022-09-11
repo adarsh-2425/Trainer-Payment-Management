4ac4a175a7a067d6fd6fbbcd8d778e56094c9905
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2022-09-11 21:01:32 +0530

    finance

--- FrontEnd/src/app/finance/financetimesheet/financetimesheet.component.ts
+++ FrontEnd/src/app/finance/financetimesheet/financetimesheet.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { TimesheetService } from 'src/app/timesheet.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+
+
+
 
 @Component({
   selector: 'app-financetimesheet',
@@ -10,11 +14,11 @@ import { Router } from '@angular/router';
 })
 export class FinancetimesheetComponent implements OnInit {
 
-  pdfs : any={
+  pdfs : any=[{
     Name:'',
     Email:'',
     pdf:''
-  }
+  }]
 
   constructor(private _timesheet : TimesheetService, 
     private http : HttpClient,  private router :Router) { }
@@ -23,6 +27,28 @@ export class FinancetimesheetComponent implements OnInit {
     this._timesheet.timesheetpdf().subscribe((data)=>{
       this.pdfs=JSON.parse(JSON.stringify(data));
   })
+  }
+
+  async  createPdf() {
+    console.log('hi');
+    
+    const pdfDoc = await PDFDocument.create()
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+  
+    const page = pdfDoc.addPage()
+    const { width, height } = page.getSize()
+    const fontSize = 30
+    page.drawText('Creating PDFs in JavaScript is awesome!', {
+      x: 50,
+      y: height - 4 * fontSize,
+      size: fontSize,
+      font: timesRomanFont,
+      color: rgb(0, 0.53, 0.71),
+    })
+    // fs.writeFileSync('./test.pdf', await pdfDoc.save());
+ const pdfBytes = await pdfDoc.save()
+ console.log(pdfBytes);
+   
   }
 
 }
